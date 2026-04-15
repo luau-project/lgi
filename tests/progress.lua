7b508b672741ccca05d4fb8f2d2ba2a96c9c64cf
luau-project <luau.project@gmail.com>
2026-04-14 22:36:59 -0300

    fix(tests): Gio.File.copy_async uses closures on newer GLib (&ge; 2.82)

--- tests/progress.lua
+++ tests/progress.lua
@@ -9,6 +9,8 @@
 --]]--------------------------------------------------------------------------
 
 local lgi = require 'lgi'
+local core = require 'lgi.core'
+local GObject = lgi.GObject
 local Gio = lgi.Gio
 local GLib = lgi.GLib
 
@@ -44,8 +46,22 @@ function progress.file_copy()
 	check_gerror(Gio.File, 'copy_finish', self, result)
 	loop:quit()
     end
-
-    src:copy_async(dst, flags, priority, cancellable,
-		   progress_callback, finish_callback)
+    --
+    -- Fixes https://github.com/lgi-devs/lgi/issues/348
+    --
+    -- Implementation for the fix was a combination
+    -- of ideas from:
+    --
+    -- 1) my own trial-error to find the transition version for GLib (2.82.0)
+    -- 2) Implementation was also partially mirrored from the LGI fork:
+    --      https://github.com/vtrlx/LuaGObject/blob/cd261460f275ea07a4b47cc0c9d0113e17f98b11/tests/progress.lua#L49-L51
+    --
+    if core.repo.GLib.check_version(2, 82, 0) then
+        src:copy_async(dst, flags, priority, cancellable,
+            progress_callback, finish_callback)
+    else
+        src:copy_async(dst, flags, priority, cancellable,
+            GObject.Closure(progress_callback), GObject.Closure(finish_callback))
+    end
     loop:run()
 end
